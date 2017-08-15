ea71381f44569d058ad8390523816319fa27e32a
JaimeYerovi <mito_yero@hotmail.com>
2017-08-15 00:02:09 -0500

    terminado de interfaces

--- ExamenPizza/api/controllers/IngredientesController.js
+++ ExamenPizza/api/controllers/IngredientesController.js
@@ -12,6 +12,52 @@ module.exports = {
 
 
 
+  crearIngredienteQuemado: function (req, res) {
+    // http://localhost:1337/Saludo/crearUsuarioQuemado
+    // /Saludo/crearUsuarioQuemado ->RELATIVE PATH
+    var parametros = req.allParams();
+    sails.log.info("Parametros", parametros);
+    //  Ejemplo para crear por parametros query
+    // http://localhost:1337/Saludo/crearUsuarioQuemado?nombres=Vicente&fechaNacimiento=2016-01-02
+    var nuevoIngrediente = {
+      nombres: parametros.nombres,
+      precio:parametros.precio
+          };
+    // 1 - Query Parameters ?nombre=Adrian&apellidos=Eguez
+    // 2 - Forms Parameters
+    /*let nuevoUsuario = {
+     nombre:"Adrian",
+     apellidos:"Eguez",
+     password:"1234",
+     correo:"1@1.com",
+     fechaNacimiento:new Date()
+     };
+     */
+    // NombreModelo.metodo(parametros).exec((err,registros)=>{})
+    Ingrediente.create(nuevoUsuario)
+      .exec(function (error, usuarioCreado) {
+        if (error) {
+          return res.serverError(error);
+        }
+        else {
+          return res.redirect("/");
+          /*
 
-};
+           Usuario.find().exec((err,usuarios)=>{
+           if(err) return res.negotiate(err);
+
+           sails.log.info("Usuarios",usuarios);
+
+           return res.view('homepage',{
+           usuarios:usuarios
+           })
+           })
+           */
+          //return res.ok(usuarioCreado);
+        }
+      });
+  },
+
+
+  };
 
